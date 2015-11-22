6a1b845fcd5306cef2f4f34273110dcd87fb0c55
shaojie.xu <shaojie.xu@infinitagroup.it>
2015-11-22 18:17:33 +0100

    initial commit

---
+++ src/main/java/it/shaojiexu/www/HappyCubeApplication.java
@@ -0,0 +1,12 @@
+package it.shaojiexu.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyCubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyCubeApplication.class, args);
+    }
+}
