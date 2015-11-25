4a6dbdfa68ea7a2e5309fc7f4d27f5d56a5b2c00
shaojie.xu <shaojie.xu@infinitagroup.it>
2015-11-25 21:29:54 +0100

    add filewriter to write out the result to file happy_cube.txt

--- src/main/java/it/shaojiexu/www/HappyCubeApplication.java
+++ src/main/java/it/shaojiexu/www/HappyCubeApplication.java
@@ -5,8 +5,13 @@ import it.shaojiexu.www.model.CubeSolution;
 import it.shaojiexu.www.service.CubeFactory;
 import it.shaojiexu.www.service.CubeService;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -26,7 +31,8 @@ public class HappyCubeApplication {
 @Component
 class CubeBuildRunner implements CommandLineRunner {
 
-	
+	private static final Logger logger = LoggerFactory.getLogger(CubeBuildRunner.class);
+
 	@Autowired
 	CubeService cubeService;
 	
@@ -36,12 +42,39 @@ class CubeBuildRunner implements CommandLineRunner {
 	@Value("${cube.color}")
 	private String cubeColor;
 	
+	@Value("${cube.file.path}")
+	private String cubeFilePath;
+	
+	@Value("${cube.file.name}")
+	private String cubeFileName;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		Cube cubeBlue = cubeFactory.getCubeByColor(cubeColor);
 		List<CubeSolution> solutions = this.cubeService.buildCube(cubeBlue);
-		solutions.forEach(solution -> System.out.println(solution));
+		
+		logger.info(String.format("******* start building %s cube ************", cubeColor));
+		
+		try{
+			
+			FileWriter cubeFileWriter = new FileWriter(cubeFilePath + File.separator +cubeFileName);
+			
+			for(int i = 0; i < solutions.size(); i++){
+				String output = String.format("Solution %d for %s cube : %s", i+1, cubeColor, solutions.get(i).toString());
+				logger.info(output);
+				cubeFileWriter.append(output);
+				cubeFileWriter.append("\n");
+			}
+			
+			cubeFileWriter.flush();
+			cubeFileWriter.close();
+			
+			logger.info(String.format("******* end building %s cube ************", cubeColor));
+
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
